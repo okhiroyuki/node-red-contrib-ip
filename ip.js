194a8e2963a748d0550fdff318e19600799e2e55
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2019-07-18 22:19:26 +0900

    add test

--- ip.js
+++ ip.js
@@ -14,7 +14,7 @@ module.exports = function (RED) {
         if((parseInt(n.timeout)) !== 5000 && (parseInt(n.timeout)) >= 5000 && (parseInt(n.timeout)) <= 10000){
             node.opt.timeout = parseInt(n.timeout);
         }
- 
+
         function getIP(name, request, args){
             return async () => {
                 try{
@@ -31,8 +31,8 @@ module.exports = function (RED) {
         if(n.publicIPv4) node.requests.push(getIP('publicIPv4', publicIp.v4, node.opt));
         if(n.publicIPv6) node.requests.push(getIP('publicIPv6', publicIp.v6, node.opt));
 
-        node.on("input", function(msg) { 
-            msg.payload = {};  
+        node.on("input", function(msg) {
+            msg.payload = {};
             (async () =>{
                 let requests = node.requests;
                 let promises = [];
@@ -40,10 +40,10 @@ module.exports = function (RED) {
                     promises.push(requests[i]());
                 }
                 let results = await Promise.all(promises);
-            
+
                 for(let i = 0; i < results.length; i++){
                     let result = results[i];
-                    if(result !== null && result !== undefined){ 
+                    if(result !== null && result !== undefined){
                         msg.payload[result[0]] = result[1];
                     }
                 }
