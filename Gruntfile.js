498a034b15eedb59f7689a64d45742047a0318fa
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-01-07 08:21:12 +0900

    feat: update packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt){
             }
         },
         inlinelint: {
-            html: ['*/*.html'],
+            html: ['*.html'],
             options: {
                 jshintrc: ".jshintrc"
             }
